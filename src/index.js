60af4f2ed3c8176d92b0334291e3d6b81a56a557
AMWilkes <amwilkes20@gmail.com>
2019-09-22 14:33:12 -0500

    scene0 camera

--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
 /*global Phaser, window*/
 import BootScene from './scenes/BootScene.js';
 import Config from './config/config.js';
-import Scene0 from './config/Scene0.js';
-import Scene1 from './config/Scene1.js';
+import Scene0 from './scenes/Scene0.js';
+//import Scene1 from './scenes/Scene1.js';
 
 class Game extends Phaser.Game {
   constructor () {
     super(Config);
     this.scene.add('Boot', BootScene);
     this.scene.add('Scene0', Scene0);
-    this.scene.add('Scene1', Scene1);
-    this.scene.start('Boot');
+    //this.scene.add('Scene1', Scene1);
+    this.scene.start('Scene0');
   }
 }
 
--- src/scenes/Scene0.js
+++ src/scenes/Scene0.js
@@ -4,4 +4,23 @@ export default class Scene0 extends Phaser.Scene {
   }
 
 
+  preload(){
+  this.centerX = this.cameras.main.width / 2;
+  this.centerY = this.cameras.main.height / 2;
+  }
+
+  create(){
+
+    //this.background = this.add.sprite(400, 300, "sky");
+    this.cameras.main.setBackgroundColor(0x008080);//for now it's a rando color
+    this.cameras.main.setBounds(0,0, 1280, 960);
+
+    var lengthBackground = 3000 - this.cameras.main.width;// total - camera.width
+
+    for(var i = 0; i < lengthBackground; i++){
+      this.cameras.x +=1;
+    }
+
+  }
+
   }
