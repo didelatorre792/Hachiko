d60d27b85d03c00a7be612c603da48fb3bee329d
Diana De La Torre <didelatorre@utexas.edu>
2019-09-22 14:33:38 -0500

    player controls and physics added

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 /*global Phaser, window*/
 import BootScene from './scenes/BootScene.js';
 import Config from './config/config.js';
+import Scene1 from './scenes/Scene1.js'
 
 class Game extends Phaser.Game {
   constructor () {
     super(Config);
     this.scene.add('Boot', BootScene);
-    this.scene.start('Boot');
+    //this.scene.add('Scene0', Scene0);
+    this.scene.add('Scene1', Scene1);
+    this.scene.start('Scene1');
   }
 }
-
 window.game = new Game();
