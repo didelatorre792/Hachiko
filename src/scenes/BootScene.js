92263d6a8d8b6a4bc4171710a8b5b399838c1296
Marissa Green <marissaaagreen@gmail.com>
2019-10-14 01:39:11 -0500

    added in park background music

--- src/scenes/BootScene.js
+++ src/scenes/BootScene.js
@@ -34,9 +34,9 @@ export default class BootScene extends Phaser.Scene {
     this.load.audio("nerfShoot", "./assets/sounds/nerfGunShot.wav");
     this.load.audio("jump", "./assets/sounds/jump.wav");
     this.load.audio("alleyBackgroundMusic", "./assets/sounds/alleyBackground.ogg");
-    this.load.audio("parkBackgroundMusic", "./assets/sounds/");
+    this.load.audio("parkBackgroundMusic", "./assets/sounds/parkMusic.mov");
   }
-  
+
 
   create (data) {
     this.anims.create({
